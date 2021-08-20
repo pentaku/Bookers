cbd6b9c363aeaf39f71826176a3cd1a52b18d7a6
pentaku <ohara1728136@gmail.com>
2021-08-20 02:12:02 +0000

    [update]

---
+++ app/controllers/todolists_controller.rb
@@ -0,0 +1,35 @@
+class TodolistsController < ApplicationController
+
+  def create
+    list = List.new(list_params)
+    list.save
+    redirect_to '/top'
+
+  end
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to '/todolists/index'
+  end
+
+
+
+  private
+  def list_params
+    params.require(:list).permit(:title, :body)
+  end
+
+end
